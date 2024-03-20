d3ceeec15dca6f57978056cab538d2ffb6b03642
Razzorback225 <thibaut.delguste@gmail.com>
2024-03-20 14:26:39 +0100

    Added missing internal references

---
+++ Software/p1wi_firmware/p1wi_firmware.ino
@@ -0,0 +1,13 @@
+#include <WiFi.h>
+#include <Preferences.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial2.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
